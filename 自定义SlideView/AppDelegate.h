a1229a6a73047d4155acd9c08cb35cf0eac7e480
MrHuanghanzhang <690612676@qq.com>
2015-07-06 13:32:10 +0800

    Initial Commit

---
+++ 自定义SlideView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义SlideView
+//
+//  Created by 黄含章 on 15/7/6.
+//  Copyright (c) 2015年 HHZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
